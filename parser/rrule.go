eeb2320e2b5e45b469f9fb6527b42ff9a700dda3
Patrick Gerber <p.gerber@iemgroup.com>
2025-01-24 13:45:16 +0100

    Rename files

--- parser/rrule.go
+++ parser/rrule.go
@@ -2,72 +2,298 @@ package parser
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/teambition/rrule-go"
 )
 
-func printTimeSlice(ts []time.Time) {
-	for _, t := range ts {
-		fmt.Println(t)
-	}
+// RecurrentDate represents an interface for recurrent date operations.
+type RecurrentDate interface {
+	Next(now time.Time) (time.Time, error)
+	Prev(now time.Time) (time.Time, error)
 }
 
-/*
-func ParseDatePattern(pattern string, now time.Time) (*rrule.RRule, error) {
-	re := regexp.MustCompile(`^(\d+|\*)\/(\d+|\*)\/(\d+|\*)(?:\s(.+)\s|\s)(\d+|\*):(\d+|\*):(\d+|\*)$`)
-	matches := re.FindStringSubmatch(pattern)
-	if matches == nil || len(matches) != 8 {
-		return nil, fmt.Errorf("Invalid date pattern: %s", pattern)
+var functionRegex = regexp.MustCompile(`^(\w+)\((.+)\)$`)
+
+func ParseRecurrentDate(pattern string) (RecurrentDate, error) {
+	recurrentDateTypes := map[string]func(string) (RecurrentDate, error){
+		"periodic": func(arg string) (RecurrentDate, error) {
+			r := RecurrentDatePeriodic{}
+			err := r.Parse(arg)
+			return r, err
+		},
+		"pattern": func(arg string) (RecurrentDate, error) {
+			r := RecurrentDatePattern{}
+			err := r.ParseFromDatePattern(arg)
+			return r, err
+		},
+		"rrule": func(arg string) (RecurrentDate, error) {
+			r := RecurrentDatePattern{}
+			err := r.ParseFromRRule(arg)
+			return r, err
+		},
 	}
 
-	year := matches[1]
-	month := matches[2]
-	day := matches[3]
-	weekdays := matches[4]
-	hour := matches[5]
-	minute := matches[6]
-	second := matches[7]
-
-	option := rrule.ROption{}
-
-	if year != "*" {
-		option.By
-	}
-	if month != "*" {
-		option.
+	// Split function name and function arguments
+	matches := functionRegex.FindStringSubmatch(pattern)
+	if matches == nil || len(matches) != 3 {
+		return nil, fmt.Errorf("error while parsing %s pattern, invalid pattern, expected form 'type(pattern)'", pattern)
 	}
 
-	time.Date(1996, 05, 5, 9, 0, 0, 0, time.UTC)
+	// Create the recurrent date object
+	createRecurrentDate, exists := recurrentDateTypes[matches[1]]
+	if !exists {
+		return nil, fmt.Errorf("error while parsing %s pattern, unknown type %s", pattern, matches[1])
+	}
+	return createRecurrentDate(matches[2])
+}
 
-	for i, match := range matches {
-		fmt.Println(i, " => ", match)
+// RecurrentDatePeriodic represents a periodic recurrent date.
+type RecurrentDatePeriodic struct {
+	Period Duration
+}
+
+func (r *RecurrentDatePeriodic) Parse(pattern string) error {
+	duration, err := ParseDuration(pattern)
+	if err != nil {
+		return err
+	}
+	r.Period = duration
+	return nil
+}
+
+// Next returns the next occurrence based on the current time.
+func (r RecurrentDatePeriodic) Next(now time.Time) (time.Time, error) {
+	return now.Add(r.Period.toDuration()), nil
+}
+
+// Prev returns the previous occurrence based on the current time.
+func (r RecurrentDatePeriodic) Prev(now time.Time) (time.Time, error) {
+	return now.Add(-r.Period.toDuration()), nil
+}
+
+// RecurrentDatePattern represents a pattern based recurrent date.
+type RecurrentDatePattern struct {
+	rule *rrule.RRule
+}
+
+func (r *RecurrentDatePattern) ParseFromDatePattern(pattern string) error {
+	rule, err := BuilRRuleFromDatePattern(pattern)
+	if err != nil {
+		return fmt.Errorf("error while parsing %s rule pattern, %v", pattern, err)
+	}
+	rule.DTStart(time.Date(2020, 01, 01, 0, 0, 0, 0, time.UTC)) //TODO: Start date must be before the current date to find the previous occurrence, see if any smarter thing can be done
+	r.rule = rule
+	return nil
+}
+
+func (r *RecurrentDatePattern) ParseFromRRule(pattern string) error {
+	rule, err := rrule.StrToRRule(pattern)
+	if err != nil {
+		return fmt.Errorf("error while parsing %s rule pattern, %v", pattern, err)
+	}
+	rule.DTStart(time.Date(2020, 01, 01, 0, 0, 0, 0, time.UTC)) //TODO: Start date must be before the current date to find the previous occurrence, see if any smarter thing can be done
+	r.rule = rule
+	return nil
+}
+
+// Next returns the next occurrence based on the current time.
+func (r RecurrentDatePattern) Next(now time.Time) (time.Time, error) {
+	//TODO: check if now is not too much in the past, before DTStart constant date
+	fmt.Println("Next: ", r.rule.String(), now)
+	next := r.rule.After(now, false)
+	if next.IsZero() {
+		return next, fmt.Errorf("no next occurrence found")
+	}
+	return next, nil
+}
+
+// Prev returns the previous occurrence based on the current time.
+func (r RecurrentDatePattern) Prev(now time.Time) (time.Time, error) {
+	//TODO: check if now is not too much in the past, before DTStart constant date
+	fmt.Println("Prev: ", r.rule.String(), now)
+	prev := r.rule.Before(now, false)
+	if prev.IsZero() {
+		return prev, fmt.Errorf("no previous occurrence found")
+	}
+	return prev, nil
+}
+
+// Take a string describing a list or a range or a mix of both and return a list of integers representing the expanded list of values
+func expandDateComponentList(pattern string) ([]int, error) {
+
+	if pattern == "*" || pattern == "" {
+		return []int{}, nil
 	}
 
-	r, _ := rrule.NewRRule(rrule.ROption{
-		Freq:       rrule.YEARLY,
-		Interval:   4,
-		Count:      3,
-		Bymonth:    []int{11},
-		Byweekday:  []rrule.Weekday{rrule.TU},
-		Bymonthday: []int{2, 3, 4, 5, 6, 7, 8},
-		Dtstart:    time.Date(1996, 05, 5, 9, 0, 0, 0, time.UTC),
-	})
+	convertToInt := func(component string) (int, error) {
+		dayMap := map[string]int{
+			"MON": 0, "TUE": 1, "WED": 2, "THU": 3, "FRI": 4, "SAT": 5, "SUN": 6,
+		}
+		monthMap := map[string]int{
+			"JAN": 1, "FEB": 2, "MAR": 3, "APR": 4, "MAY": 5, "JUN": 6,
+			"JUL": 7, "AUG": 8, "SEP": 9, "OCT": 10, "NOV": 11, "DEC": 12,
+		}
 
-}*/
+		day, exists := dayMap[strings.ToUpper(component)]
+		if exists {
+			return day, nil
+		}
+		month, exists := monthMap[strings.ToUpper(component)]
+		if exists {
+			return month, nil
+		}
+		number, err := strconv.Atoi(component)
+		if err != nil {
+			return 0, fmt.Errorf("error while parsing %s date component, invalid date, %v", component, err)
+		}
+		return number, nil
+	}
 
-func Toto() {
+	components := strings.Split(pattern, ",")
+	var output []int
+	for _, component := range components {
+		fmt.Println("handling component", component)
 
-	r, _ := rrule.NewRRule(rrule.ROption{
-		Freq:       rrule.YEARLY,
-		Interval:   4,
-		Count:      3,
-		Bymonth:    []int{11},
-		Byweekday:  []rrule.Weekday{rrule.TU},
-		Bymonthday: []int{2, 3, 4, 5, 6, 7, 8},
-		Dtstart:    time.Date(1996, 05, 5, 9, 0, 0, 0, time.UTC),
-	})
+		if strings.Contains(component, "-") {
+			limits := strings.Split(component, "-")
+			if len(limits) != 2 {
+				return nil, fmt.Errorf("error while parsing %s date component, invalid range, expected form 'start-end'", component)
+			}
+			start, err := convertToInt(limits[0])
+			if err != nil {
+				return nil, err
+			}
+			end, err := convertToInt(limits[1])
+			if err != nil {
+				return nil, err
+			}
+			if start > end || end-start > 35 {
+				return nil, fmt.Errorf("error while parsing %s date component, invalid range, start is greater than end or range is too large", component)
+			}
+			for i := start; i <= end; i++ {
+				output = append(output, i)
+			}
+		} else {
+			day, err := convertToInt(component)
+			if err != nil {
+				return nil, err
+			}
+			output = append(output, day)
+		}
+	}
+	return output, nil
 
-	fmt.Println(r.Before(time.Now(), true))
-	printTimeSlice(r.All())
+}
+
+// Regular expression to parse a date pattern in the form of "<yyyy/>mm/dd <weekdays> hh:mm<:ss> <extra>"
+var rrule_regex = regexp.MustCompile(`^(?:([\d\-,*]+)\/)?([\d\-,*]+)\/([\d\-,*]+)\s+(?:([\w\-,*]*)\s+)?([\d\-,*]+):([\d\-,*]+)(?::([\d\-,*]*))?(?: (.*))?$`)
+
+// BuilRRuleFromDatePattern takes a date pattern in the form of "<yyyy/>mm/dd <weekdays> hh:mm<:ss> <extra>" and returns a RRule object
+func BuilRRuleFromDatePattern(pattern string) (*rrule.RRule, error) {
+
+	//Parse the date pattern
+	matches := rrule_regex.FindStringSubmatch(pattern)
+	if matches == nil || len(matches) != 9 {
+		return nil, fmt.Errorf("error while parsing %s pattern, invalid pattern, expected 'yyyy/mm/dd <weekdays> hh:mm:ss <extra>'", pattern)
+	}
+	extra_str := matches[8]
+
+	fmt.Println("\nHandling pattern: ", pattern)
+	fmt.Printf("%#v\n", matches)
+
+	// Find the frequency looking for the first "*" field
+	frequency := rrule.YEARLY
+	frequencyList := []rrule.Frequency{rrule.YEARLY, rrule.MONTHLY, rrule.WEEKLY, rrule.DAILY, rrule.HOURLY, rrule.MINUTELY, rrule.SECONDLY}
+	matchFrequencyList := []string{matches[1], matches[2], matches[4], matches[3], matches[5], matches[6], matches[7]}
+	found := false
+	for i, match := range matchFrequencyList {
+		if match == "*" {
+			frequency = frequencyList[i]
+			found = true
+		}
+	}
+	if !found {
+		return nil, fmt.Errorf("error while parsing '%s' pattern, no '*' field found to determine frequency", pattern)
+	}
+
+	rropt := &rrule.ROption{}
+	if extra_str != "" {
+		var err error
+		extra_str = "FREQ=" + frequency.String() + ";" + extra_str //Add the frequency to the extra part as this is mendatory accordingly to RFC 5545
+		rropt, err = rrule.StrToROption(extra_str)
+		if err != nil {
+			return nil, fmt.Errorf("error while parsing '%s' pattern, invalid extra part, %v", pattern, err)
+		}
+	}
+	rropt.Freq = frequency
+	rropt.Wkst = rrule.MO
+
+	//Decode month
+	byMonth, err := expandDateComponentList(matches[2])
+	if err != nil {
+		return nil, err
+	}
+	if len(byMonth) > 0 {
+		rropt.Bymonth = byMonth
+	}
+	//Decode day
+	byMonthDay, err := expandDateComponentList(matches[3])
+	if err != nil {
+		return nil, err
+	}
+	if len(byMonthDay) > 0 {
+		rropt.Bymonthday = byMonthDay
+	}
+	//Decode weekday
+	byWeekday, err := expandDateComponentList(matches[4])
+	if err != nil {
+		return nil, err
+	}
+	if len(byWeekday) > 0 {
+		//Convert list of int to list of rrule.Weekday
+		var weekdays []rrule.Weekday
+		weekdayList := []rrule.Weekday{rrule.MO, rrule.TU, rrule.WE, rrule.TH, rrule.FR, rrule.SA, rrule.SU}
+		for _, day := range byWeekday {
+			weekdays = append(weekdays, weekdayList[day])
+		}
+		rropt.Byweekday = weekdays
+	}
+	//Decode hour
+	byHour, err := expandDateComponentList(matches[5])
+	if err != nil {
+		return nil, err
+	}
+	if len(byHour) > 0 {
+		rropt.Byhour = byHour
+	}
+	//Decode minute
+	byMinute, err := expandDateComponentList(matches[6])
+	if err != nil {
+		return nil, err
+	}
+	if len(byMinute) > 0 {
+		rropt.Byminute = byMinute
+	}
+	//Decode second
+	bySecond, err := expandDateComponentList(matches[7])
+	if err != nil {
+		return nil, err
+	}
+	if len(bySecond) > 0 {
+		rropt.Bysecond = bySecond
+	}
+
+	//Create the RRule
+	rrule, err := rrule.NewRRule(*rropt)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(rrule.String())
+
+	return rrule, nil
 }
--- parser/rrule_test.go
+++ parser/rrule_test.go
@@ -1,9 +1,605 @@
 package parser
 
-/*
-func TestRRuleParsing(t *testing.T) {
-	ParseDatePattern("* / * / * MO,TU *:*:*")
-	ParseDatePattern("2024/12/24 *:*:*")
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
+func TestParseDuration(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected Duration
+		hasError bool
+	}{
+		{"1w", Duration(7 * 24 * time.Hour), false},
+		{"2d", Duration(2 * 24 * time.Hour), false},
+		{"3h", Duration(3 * time.Hour), false},
+		{"4m", Duration(4 * time.Minute), false},
+		{"5s", Duration(5 * time.Second), false},
+		{"1w2d3h4m5s", Duration(7*24*time.Hour + 2*24*time.Hour + 3*time.Hour + 4*time.Minute + 5*time.Second), false},
+		{"2d3h4m5s", Duration(2*24*time.Hour + 3*time.Hour + 4*time.Minute + 5*time.Second), false},
+		{"3h4m5s", Duration(3*time.Hour + 4*time.Minute + 5*time.Second), false},
+		{"4m5s", Duration(4*time.Minute + 5*time.Second), false},
+		{"5s", Duration(5 * time.Second), false},
+		{"1w2h", Duration(7*24*time.Hour + 2*time.Hour), false}, // Valid order
+		{"2d1w", 0, true}, // Invalid order
+		{"3h2d", 0, true}, // Invalid order
+		{"4m3h", 0, true}, // Invalid order
+		{"5s4m", 0, true}, // Invalid order
+		{"", 0, true},     // Empty string
+		{"1x", 0, true},   // Unknown unit
+	}
+
+	for _, test := range tests {
+		result, err := ParseDuration(test.input)
+		if (err != nil) != test.hasError {
+			t.Errorf("ParseDuration(%q) error = %v, wantErr %v", test.input, err, test.hasError)
+			continue
+		}
+		if result != test.expected {
+			t.Errorf("ParseDuration(%q) = %v, want %v", test.input, result, test.expected)
+		}
+	}
 }
-*/
+
+func TestExpandDateComponentList(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected []int
+		hasError bool
+	}{
+		{"1,2,3", []int{1, 2, 3}, false},
+		{"1-3", []int{1, 2, 3}, false},
+		{"1,2-4,5", []int{1, 2, 3, 4, 5}, false},
+		{"MON,TUE,WED", []int{0, 1, 2}, false},
+		{"1,MON,3", []int{1, 0, 3}, false},
+		{"1-3,5-7", []int{1, 2, 3, 5, 6, 7}, false},
+		{"JAN,Feb,MAR", []int{1, 2, 3}, false},
+		{"1-3,5,7-9", []int{1, 2, 3, 5, 7, 8, 9}, false},
+		{"1,3-5,7", []int{1, 3, 4, 5, 7}, false},
+		{"1-5,10-15", []int{1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15}, false},
+		{"1-3,5-7,9-11,13-15", []int{1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15}, false},
+		{"", []int{}, false},               // Empty string
+		{"1,,2", nil, true},                // Double comma
+		{"1-2-3", nil, true},               // Invalid range
+		{"1-2,", nil, true},                // Trailing comma
+		{"1-2,3-", nil, true},              // Trailing dash
+		{"1-2,3-abc", nil, true},           // Invalid character
+		{"1-2,3-1000000000000", nil, true}, // Out of range
+	}
+
+	for _, test := range tests {
+		result, err := expandDateComponentList(test.input)
+		if (err != nil) != test.hasError {
+			t.Errorf("ExpandDateComponentList(%q) error = %v, wantErr %v", test.input, err, test.hasError)
+			continue
+		}
+		if !equal(result, test.expected) {
+			t.Errorf("ExpandDateComponentList(%q) = %v, want %v", test.input, result, test.expected)
+		}
+	}
+}
+
+func equal(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestRecurrentDatePeriodic(t *testing.T) {
+	tests := []struct {
+		pattern      string
+		now          time.Time
+		expectedNext []time.Time
+		expectedPrev []time.Time
+	}{
+		{
+			"1h30m",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			[]time.Time{
+				time.Date(2023, 10, 1, 1, 30, 0, 0, time.UTC),
+				time.Date(2023, 10, 1, 3, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 1, 4, 30, 0, 0, time.UTC),
+			},
+			[]time.Time{
+				time.Date(2023, 9, 30, 22, 30, 0, 0, time.UTC),
+				time.Date(2023, 9, 30, 21, 0, 0, 0, time.UTC),
+				time.Date(2023, 9, 30, 19, 30, 0, 0, time.UTC),
+			},
+		},
+		{
+			"2h",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			[]time.Time{
+				time.Date(2023, 10, 1, 2, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 1, 4, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 1, 6, 0, 0, 0, time.UTC),
+			},
+			[]time.Time{
+				time.Date(2023, 9, 30, 22, 0, 0, 0, time.UTC),
+				time.Date(2023, 9, 30, 20, 0, 0, 0, time.UTC),
+				time.Date(2023, 9, 30, 18, 0, 0, 0, time.UTC),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		fmt.Println("Testing periodic", test.pattern)
+		r := &RecurrentDatePeriodic{}
+		err := r.Parse(test.pattern)
+		if err != nil {
+			t.Fatalf("Parse failed: %v", err)
+		}
+
+		now := test.now
+		for _, expectedNext := range test.expectedNext {
+			next, err := r.Next(now)
+			if err != nil {
+				t.Fatalf("Next failed: %v", err)
+			}
+			if !next.Equal(expectedNext) {
+				t.Errorf("Next() = %v, want %v", next, expectedNext)
+			}
+			now = next
+		}
+
+		now = test.now
+		for _, expectedPrev := range test.expectedPrev {
+			prev, err := r.Prev(now)
+			if err != nil {
+				t.Fatalf("Prev failed: %v", err)
+			}
+			if !prev.Equal(expectedPrev) {
+				t.Errorf("Prev() = %v, want %v", prev, expectedPrev)
+			}
+			now = prev
+		}
+	}
+}
+
+func TestBuilRRuleFromDatePattern(t *testing.T) {
+
+	tests := []struct {
+		pattern       string
+		expectedrrule string
+		hasError      bool
+	}{
+		{
+			"2023/10/* MON 12:00:00",
+			"FREQ=DAILY;BYMONTH=10;BYDAY=MO;BYHOUR=12;BYMINUTE=0;BYSECOND=0",
+			false,
+		},
+		{
+			"2023/*/* Mon-Fri 12:00:00",
+			"FREQ=DAILY;BYDAY=MO,TU,WE,TH,FR;BYHOUR=12;BYMINUTE=0;BYSECOND=0",
+			false,
+		},
+		{
+			"2023/9-10/* Mon,Fri,SUN 12:00:00",
+			"FREQ=DAILY;BYMONTH=9,10;BYDAY=MO,FR,SU;BYHOUR=12;BYMINUTE=0;BYSECOND=0",
+			false,
+		},
+		{
+			"2023/*/* 12:00:00",
+			"FREQ=DAILY;BYHOUR=12;BYMINUTE=0;BYSECOND=0",
+			false,
+		},
+		{
+			"2023/10/* * 12,23:00",
+			"FREQ=DAILY;BYMONTH=10;BYHOUR=12,23;BYMINUTE=0",
+			false,
+		},
+		{
+			"2023/10/* 17:00",
+			"FREQ=DAILY;BYMONTH=10;BYHOUR=17;BYMINUTE=0",
+			false,
+		},
+		{
+			"2023/*/* Mon,FRI 12:00",
+			"FREQ=DAILY;BYDAY=MO,FR;BYHOUR=12;BYMINUTE=0",
+			false,
+		},
+		{
+			"2023/*/* Mon,FRI 12:00 COUNT=5",
+			"FREQ=DAILY;COUNT=5;BYDAY=MO,FR;BYHOUR=12;BYMINUTE=0",
+			false,
+		},
+		{
+			"2023/10/01 Mon 12:00:00",
+			"",
+			true,
+		},
+		{
+			"2023/10/01 Mon *:*:00",
+			"FREQ=MINUTELY;BYMONTH=10;BYMONTHDAY=1;BYDAY=MO;BYSECOND=0",
+			false,
+		},
+		{
+			"2023/10/01 Mon 12:00:00:00",
+			"",
+			true,
+		},
+		{
+			"2023/10/01 Mon 12:00:xx",
+			"",
+			true,
+		},
+		{
+			"2023/10/01 Mon 12:xx:00",
+			"",
+			true,
+		},
+		{
+			"2023/10/01 Mon xx:00:00",
+			"",
+			true,
+		},
+	}
+
+	for _, test := range tests {
+		fmt.Println("Testing pattern", test.pattern)
+		rrule, err := BuilRRuleFromDatePattern(test.pattern)
+		if (err != nil) != test.hasError {
+			t.Errorf("BuilRRuleFromDatePattern(%q) error = %v, wantErr %v", test.pattern, err, test.hasError)
+			continue
+		}
+		if err != nil {
+			continue
+		}
+		if rrule.String() != test.expectedrrule {
+			t.Errorf("BuilRRuleFromDatePattern(%q) = %v, want %v", test.pattern, rrule, test.expectedrrule)
+		}
+	}
+}
+
+func TestRecurrentDatePattern(t *testing.T) {
+	tests := []struct {
+		pattern      string
+		now          time.Time
+		expectedNext []time.Time
+		expectedPrev []time.Time
+		hasError     bool
+	}{
+		{
+			"2023/10/* MON 12:00:00",
+			time.Date(2023, 10, 16, 0, 0, 0, 0, time.UTC),
+			[]time.Time{
+				time.Date(2023, 10, 16, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 23, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 30, 12, 0, 0, 0, time.UTC),
+			},
+			[]time.Time{
+				time.Date(2023, 10, 9, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 2, 12, 0, 0, 0, time.UTC),
+			},
+			false,
+		},
+		{
+			"2023/*/* Mon-Fri 12:00:00",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			[]time.Time{
+				time.Date(2023, 10, 2, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 3, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 4, 12, 0, 0, 0, time.UTC),
+			},
+			[]time.Time{
+				time.Date(2023, 9, 29, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 9, 28, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 9, 27, 12, 0, 0, 0, time.UTC),
+			},
+			false,
+		},
+		{
+			"2023/9-10/* Mon,Fri,SUN 12:00:00",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			[]time.Time{
+				time.Date(2023, 10, 1, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 2, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 6, 12, 0, 0, 0, time.UTC),
+			},
+			[]time.Time{
+				time.Date(2023, 9, 29, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 9, 25, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 9, 24, 12, 0, 0, 0, time.UTC),
+			},
+			false,
+		},
+		{
+			"2023/*/* 12,14,16,18,20,22:00:00",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			[]time.Time{
+				time.Date(2023, 10, 1, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 1, 14, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 1, 16, 0, 0, 0, time.UTC),
+			},
+			[]time.Time{
+				time.Date(2023, 9, 30, 22, 0, 0, 0, time.UTC),
+				time.Date(2023, 9, 30, 20, 0, 0, 0, time.UTC),
+				time.Date(2023, 9, 30, 18, 0, 0, 0, time.UTC),
+			},
+			false,
+		},
+		{
+			"2023/10/* * 12,23:00",
+			time.Date(2023, 10, 8, 16, 0, 0, 0, time.UTC),
+			[]time.Time{
+				time.Date(2023, 10, 8, 23, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 9, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 9, 23, 0, 0, 0, time.UTC),
+			},
+			[]time.Time{
+				time.Date(2023, 10, 8, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 7, 23, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 7, 12, 0, 0, 0, time.UTC),
+			},
+			false,
+		},
+		{
+			"2023/10/* 17:00",
+			time.Date(2023, 10, 5, 0, 0, 0, 0, time.UTC),
+			[]time.Time{
+				time.Date(2023, 10, 5, 17, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 6, 17, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 7, 17, 0, 0, 0, time.UTC),
+			},
+			[]time.Time{
+				time.Date(2023, 10, 4, 17, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 3, 17, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 2, 17, 0, 0, 0, time.UTC),
+			},
+			false,
+		},
+		{
+			"2023/*/* Mon,FRI 12:00",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			[]time.Time{
+				time.Date(2023, 10, 2, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 6, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 10, 9, 12, 0, 0, 0, time.UTC),
+			},
+			[]time.Time{
+				time.Date(2023, 9, 29, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 9, 25, 12, 0, 0, 0, time.UTC),
+				time.Date(2023, 9, 22, 12, 0, 0, 0, time.UTC),
+			},
+			false,
+		},
+		{
+			"2023/10/01 Mon 12:00:00:00",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			[]time.Time{},
+			[]time.Time{},
+			true,
+		},
+		{
+			"2023/10/01 Mon 12:00:xx",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			[]time.Time{},
+			[]time.Time{},
+			true,
+		},
+		{
+			"2023/10/01 Mon 12:xx:00",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			[]time.Time{},
+			[]time.Time{},
+			true,
+		},
+		{
+			"2023/10/01 Mon xx:00:00",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			[]time.Time{},
+			[]time.Time{},
+			true,
+		},
+	}
+
+	for _, test := range tests {
+		r := &RecurrentDatePattern{}
+		fmt.Println("Testing pattern", test.pattern)
+		err := r.ParseFromDatePattern(test.pattern)
+		if (err != nil) != test.hasError {
+			t.Errorf("ParseRecurrentDatePattern(%q) error = %v, wantErr %v", test.pattern, err, test.hasError)
+			continue
+		}
+		if err != nil {
+			continue
+		}
+
+		now := test.now
+		for _, expectedNext := range test.expectedNext {
+			next, err := r.Next(now)
+			if err != nil {
+				t.Fatalf("Next failed: %v", err)
+			}
+			if !next.Equal(expectedNext) {
+				t.Errorf("Next(%v) = %v, want %v", now, next, expectedNext)
+			}
+			now = next
+		}
+
+		now = test.now
+		for _, expectedPrev := range test.expectedPrev {
+			prev, err := r.Prev(now)
+			if (err != nil) != (expectedPrev == time.Unix(0, 0)) {
+				t.Fatalf("Prev failed: %v", err)
+			}
+			if (expectedPrev != time.Unix(0, 0)) && !prev.Equal(expectedPrev) {
+				t.Errorf("Prev(%v) = %v, want %v", now, prev, expectedPrev)
+			}
+			now = prev
+		}
+	}
+}
+
+func TestParseRecurrentDate(t *testing.T) {
+	tests := []struct {
+		pattern      string
+		now          time.Time
+		expectedNext time.Time
+		expectedPrev time.Time
+		hasError     bool
+	}{
+		{
+			"periodic(1h30m)",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(2023, 10, 1, 1, 30, 0, 0, time.UTC),
+			time.Date(2023, 9, 30, 22, 30, 0, 0, time.UTC),
+			false,
+		},
+		{
+			"pattern(2023/10/* MON 12:00:00)",
+			time.Date(2023, 10, 16, 0, 0, 0, 0, time.UTC),
+			time.Date(2023, 10, 16, 12, 0, 0, 0, time.UTC),
+			time.Date(2023, 10, 9, 12, 0, 0, 0, time.UTC),
+			false,
+		},
+		{
+			"pattern(2023/*/* Mon-Fri 12:00:00)",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(2023, 10, 2, 12, 0, 0, 0, time.UTC),
+			time.Date(2023, 9, 29, 12, 0, 0, 0, time.UTC),
+			false,
+		},
+		{
+			"pattern(2023/9-10/* Mon,Fri,SUN 12:00:00)",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(2023, 10, 1, 12, 0, 0, 0, time.UTC),
+			time.Date(2023, 9, 29, 12, 0, 0, 0, time.UTC),
+			false,
+		},
+		{
+			"pattern(2023/*/* 12:00:00)",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(2023, 10, 1, 12, 0, 0, 0, time.UTC),
+			time.Date(2023, 9, 30, 12, 0, 0, 0, time.UTC),
+			false,
+		},
+		{
+			"pattern(2023/10/01 * 12,23:00)",
+			time.Date(2023, 10, 1, 16, 0, 0, 0, time.UTC),
+			time.Date(2023, 10, 1, 23, 0, 0, 0, time.UTC),
+			time.Date(2023, 10, 1, 12, 0, 0, 0, time.UTC),
+			false,
+		},
+		{
+			"pattern(2023/10/* 17:00)",
+			time.Date(2023, 10, 3, 0, 0, 0, 0, time.UTC),
+			time.Date(2023, 10, 3, 17, 0, 0, 0, time.UTC),
+			time.Date(2023, 10, 2, 17, 0, 0, 0, time.UTC),
+			false,
+		},
+		{
+			"pattern(2023/*/* Mon,FRI 12:00)",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(2023, 10, 2, 12, 0, 0, 0, time.UTC),
+			time.Date(2023, 9, 29, 12, 0, 0, 0, time.UTC),
+			false,
+		},
+		{
+			"invalid(1h30m)",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			time.Time{},
+			time.Time{},
+			true,
+		},
+		{
+			"periodic(invalid)",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			time.Time{},
+			time.Time{},
+			true,
+		},
+		{
+			"pattern(invalid)",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			time.Time{},
+			time.Time{},
+			true,
+		},
+		{
+			"",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			time.Time{},
+			time.Time{},
+			true,
+		},
+		{
+			"()",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			time.Time{},
+			time.Time{},
+			true,
+		},
+		{
+			"periodic()",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			time.Time{},
+			time.Time{},
+			true,
+		},
+		{
+			"pattern()",
+			time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC),
+			time.Time{},
+			time.Time{},
+			true,
+		},
+		{
+			"(1h30m)",
+			time.Time{},
+			time.Time{},
+			time.Time{},
+			true,
+		},
+		{
+			"(0 0 * * *)",
+			time.Time{},
+			time.Time{},
+			time.Time{},
+			true,
+		},
+		{
+			"(2023/10/* MON 12:00:00)",
+			time.Time{},
+			time.Time{},
+			time.Time{},
+			true,
+		},
+	}
+
+	for _, test := range tests {
+		fmt.Println("Testing pattern", test.pattern)
+		recurrentDate, err := ParseRecurrentDate(test.pattern)
+		if (err != nil) != test.hasError {
+			t.Errorf("ParseRecurrentDate(%q) error = %v, wantErr %v", test.pattern, err, test.hasError)
+			continue
+		}
+		if err != nil {
+			continue
+		}
+
+		next, err := recurrentDate.Next(test.now)
+		if err != nil {
+			t.Fatalf("Next failed: %v", err)
+		}
+		if !next.Equal(test.expectedNext) {
+			t.Errorf("Next(%v) = %v, want %v", test.now, next, test.expectedNext)
+		}
+
+		prev, err := recurrentDate.Prev(test.now)
+		if err != nil {
+			t.Fatalf("Prev failed: %v", err)
+		}
+		if !prev.Equal(test.expectedPrev) {
+			t.Errorf("Prev(%v) = %v, want %v", test.now, prev, test.expectedPrev)
+		}
+	}
+}
